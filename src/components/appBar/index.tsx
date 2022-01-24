0f4a4e486e7d4fac529aaa53ee0cfe67e89ce5fe
Abimael De Oliveira Ferreira <46110587+thebinario@users.noreply.github.com>
2022-01-24 03:37:02 +0000

    ajustando appBar e slider

---
+++ src/components/appBar/index.tsx
@@ -0,0 +1,113 @@
+import { ReactNode } from 'react';
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+	HStack
+} from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+
+const Links = [
+  {
+    route: "/",
+    name: "Inicio"
+  },
+  {
+    route: "claves",
+    name: "Claves"
+  },
+];
+
+const NavLink = ({ children }: { children: any }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('red.200', 'green.700'),
+    }}
+    href={children?.route}>
+    {children?.name}
+  </Link>
+);
+
+export default function AppBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box>Logo</Box>
+
+					          <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ base: 'none', md: 'flex' }}>
+            <>
+              {Links.map((link) => (
+                <NavLink key={link.route}>{link}</NavLink>
+                // <Link href={link.route} key={link.route}>{link.name}</Link>
+              ))}
+            </>
+
+          </HStack>
+
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
+                  />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import AppBar from '../components/appBar/index.tsx'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+	    <ChakraProvider>
+      <ColorModeScript initialColorMode='light'/>
+			<AppBar></AppBar>
+      <Component {...pageProps} />
+    </ChakraProvider>
+	)
 }
 
 export default MyApp
